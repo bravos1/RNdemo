0e23a3eff06b4580ed6e3a3ff14341c65d62d1fa
linjh5670 <linjh@milesight.com>
2022-03-22 09:57:58 +0800

    选中 demo 实现

--- src/Demo/index.tsx
+++ src/Demo/index.tsx
@@ -2,4 +2,3 @@ export {default as themeDemo} from './themeDemo';
 export {default as arrayListDemo} from './arrayListDemo';
 export {default as cameraDemo} from './cameraDemo';
 export {default as Demo} from './Demo'
-export {default as gestureDemo} from './gestureDemo'

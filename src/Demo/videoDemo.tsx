729ed148353ca42e446feedae564b8914b324ebb
linjh5670 <linjh@milesight.com>
2022-05-07 17:24:30 +0800

    test

--- src/Demo/videoDemo.tsx
+++ src/Demo/videoDemo.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Box, Text } from 'native-base'
-
 import Video from 'react-native-video';
+import * as testVideo from "../assets/video"
+import { StyleSheet } from 'react-native';
 const videoDemo = () => {
+  const videoRef = useRef(null)
+
   return (
     <Box>
-      <Text>sssss</Text>
+    <Text>sss</Text>
+    <Video source={{uri: "https://www.tootootool.com/wp-content/uploads/2020/11/SampleVideo_176x144_5mb.3gp"}}   // Can be a URL or a local file.
+       style={styles.backgroundVideo} 
+       repeat={true}
+       resizeMode={"contain"}
+       />
     </Box>
   )
 }
 
 export default videoDemo
+
+var styles = StyleSheet.create({
+  backgroundVideo: {
+    width:200,
+    height:200,
+  },
+});
---
+++ src/assets/video/index.js
@@ -0,0 +1,3 @@
+export { default as flv } from './flv.flv';
+export { default as mkv } from './mkv.mkv';
+export { default as gp3 } from './3gp.3gp';

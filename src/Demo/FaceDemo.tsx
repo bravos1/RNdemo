2fda4a1ada2ea6f331134505aadddfa7efd77b7f
linjh5670 <linjh@milesight.com>
2022-06-13 15:55:38 +0800

    texs

---
+++ src/Demo/FaceDemo.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Button } from 'native-base';
+
+const FaceDemo = () => {
+	return (
+		<View>
+			<Text>FaceDemo</Text>
+			<Button>xxx</Button>
+		</View>
+	);
+};
+
+export default FaceDemo;

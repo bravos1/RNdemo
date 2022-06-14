638cde7e43222e402f32025c690577820b97e146
linjh5670 <linjh@milesight.com>
2022-06-14 10:38:49 +0800

    test

--- src/Demo/index.tsx
+++ src/Demo/index.tsx
@@ -1,6 +1,5 @@
 export {default as themeDemo} from './themeDemo';
 export {default as arrayListDemo} from './arrayListDemo';
-export {default as cameraDemo} from './cameraDemo';
 export {default as Demo} from './Demo';
 export {default as video} from './videoDemo';
 export {default as StyleDemo} from './StyleDemo';

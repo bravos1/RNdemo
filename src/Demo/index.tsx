e6499797c52e3a7e61754a20a100c93cecd00627
linjh5670 <linjh@milesight.com>
2022-06-21 14:37:33 +0800

    添加facedemo base

--- src/Demo/index.tsx
+++ src/Demo/index.tsx
@@ -2,3 +2,4 @@ export {default as themeDemo} from './themeDemo';
 export {default as arrayListDemo} from './arrayListDemo';
 export {default as cameraDemo} from './cameraDemo';
 export {default as Demo} from './Demo'
+export {default as FaceDemo} from './FaceDemo'
